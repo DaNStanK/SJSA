d98519eafc424282ab86789bb8d6a9699e4dd986
DanStanK <daniel.stankovski@gmail.com>
2022-07-05 16:04:41 +0200

    uploading files

--- homework/node_js_link_calculator_anotherExample/index.js
+++ homework/node_js_link_calculator_anotherExample/index.js
@@ -1,1 +1,81 @@
-/home/semos/Documents/Daniel_repo/Practice/nodejs/domasna/c7/index.js
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const fileRead = (filename) => {
+  return new Promise((success, fail) => {
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        return fail(err);
+      }
+      return success(data);
+    });
+  });
+};
+
+const findResult = (request, htmlContent) => {
+  const qs = url.parse(request.url, true).query;
+  let [operator, _] = request.url.split("?");
+  operator = operator.replace(/\//, "");
+  let result = "";
+
+  if (operator === "divide" && qs.b === "0") {
+    result =
+      "No number is divideable with 0! Choose another number not equal to zero.";
+    return (htmlContent = htmlContent.replace(/{{res}}/, result));
+  } else {
+    switch (operator) {
+      case "plus":
+        result = `${Number(qs.a) + Number(qs.b)}`;
+        break;
+      case "minus":
+        result = `${Number(qs.a) - Number(qs.b)}`;
+        break;
+      case "multiply":
+        result = `${Number(qs.a) * Number(qs.b)}`;
+        break;
+      case "divide":
+        result = `${Number(qs.a) / Number(qs.b)}`;
+        break;
+      default:
+        result = "You have not put a proper operator in the URL!";
+        break;
+    }
+    return (htmlContent = htmlContent.replace(/{{res}}/, result));
+  }
+};
+
+const pages = {
+  "/plus": async (req, res) => {
+    let content = await fileRead("./index.html");
+    res.end(findResult(req, content));
+  },
+  "/minus": async (req, res) => {
+    let content = await fileRead("./index.html");
+    res.end(findResult(req, content));
+  },
+  "/multiply": async (req, res) => {
+    let content = await fileRead("./index.html");
+    res.end(findResult(req, content));
+  },
+  "/divide": async (req, res) => {
+    let content = await fileRead("./index.html");
+    res.end(findResult(req, content));
+  },
+};
+
+const server = http
+  .createServer((req, res) => {
+    // query string parameters // GET parameters
+    // http://localhost:8080/users?a=10&b=5
+    // /users?a=10&b=5
+    // /users                                     a=10&b=5
+
+    let [path, _] = req.url.split("?");
+    if (pages[path]) {
+      pages[path](req, res);
+    } else {
+      res.end("");
+    }
+  })
+  .listen(8080);
