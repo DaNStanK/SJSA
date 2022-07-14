2c51437ffb7c2d0873d86e8e258f913bef20876a
DaNStanK <daniel.stankovski@gmail.com>
2022-07-14 17:38:18 +0200

    ejs_nodejs_calculator

---
+++ homework/ejs_nodejs_calculator/index.js
@@ -0,0 +1,43 @@
+let express = require("express");
+let port = 8080;
+let app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/calculator", (req, res) => {
+  try {
+    res.render("calculator_input");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.post("/calculator-result", (req, res) => {
+  try {
+    let output;
+    let operator = req.body.operation;
+    switch (operator) {
+      case "+":
+        output = Number(req.body.number_1) + Number(req.body.number_2);
+        break;
+      case "-":
+        output = Number(req.body.number_1) - Number(req.body.number_2);
+        break;
+      case "/":
+        output = Number(req.body.number_1) / Number(req.body.number_2);
+        break;
+      case "*":
+        output = Number(req.body.number_1) * Number(req.body.number_2);
+        break;
+    }
+    res.render("calculator_output", { result: output });
+  } catch (error) {
+    res.send(err);
+  }
+});
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log(`Server started on port ${port}`);
+});
